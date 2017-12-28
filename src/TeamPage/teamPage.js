f1cd670be824e0b2a465b27c86b1004102cd9ebc
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-12-28 15:56:09 -0500

    Created team page with list

---
+++ src/TeamPage/teamPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import List from '../Lists/list.js';
+
+export default class TeamPage extends Component {
+    render() {
+        return (
+            <List />
+        );
+    }
+}
