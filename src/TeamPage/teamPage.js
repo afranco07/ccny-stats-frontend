a403905b9b4287e238f66dea686f2fa90dce9e19
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-12-28 16:13:23 -0500

    Added AddButton to TeamPage

--- src/TeamPage/teamPage.js
+++ src/TeamPage/teamPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import List from '../Lists/list.js';
+import AddButton from '../AddButton/addButton.js';
 
 export default class TeamPage extends Component {
     render() {
         return (
-            <List />
+            <div>
+                <AddButton buttonText={"Team"} />
+                <List />
+            </div>
         );
     }
 }
