ae240cd8c268390a0d48c046f2d8dc71a0e657db
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-12-28 15:55:18 -0500

    Created List component to use for teams and players

---
+++ src/Lists/list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class List extends Component {
+    render() {
+        return (
+            <div className="list-group">
+                <Link to="#ccny" className="list-group-item list-group-item-action">CCNY</Link>
+                <Link to="#baruch" className="list-group-item list-group-item-action">Baruch</Link>
+            </div>
+        );
+    }
+}
