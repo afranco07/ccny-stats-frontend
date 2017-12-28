dfca3e324c0a7d90c79206db7892463c8a56a185
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-12-27 21:31:06 -0500

    Changed Link components to NavLink components for navbar highlighting

--- src/NavBar/navbar.js
+++ src/NavBar/navbar.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default class NavBar extends Component {
      render() {
           return (
                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <Link to="/" className="navbar-brand">CCNY Stats</Link>
+                    <NavLink to="/" className="navbar-brand">CCNY Stats</NavLink>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                          <span className="navbar-toggler-icon"></span>
                     </button>
                
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                          <ul className="navbar-nav mr-auto">
-                              <li className="nav-item active">
-                                   <Link to="/" className="nav-link">Dashboard</Link>
+                              <li className="nav-item">
+                                   <NavLink exact to="/" className="nav-link">Dashboard</NavLink>
                               </li>
                               <li className="nav-item">
-                                   <Link to="#players" className="nav-link">Players</Link>
+                                   <NavLink to="#players" className="nav-link">Players</NavLink>
                               </li>
                               <li className="nav-item">
-                                   <Link to="/testpage" className="nav-link">Test Link</Link>
+                                   <NavLink to="/testpage" className="nav-link">Test Link</NavLink>
                               </li>
                          </ul>
                          <form className="form-inline my-2 my-lg-0">
