4b5cde820940faaf84d28681cd3adcdf8dd069aa
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-12-28 16:12:15 -0500

    Added styling to list, centered on page

--- src/Lists/list.js
+++ src/Lists/list.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './listStyles.css';
 
 export default class List extends Component {
     render() {
         return (
-            <div className="list-group">
+            <div className="list-group" id="list-width">
                 <Link to="#ccny" className="list-group-item list-group-item-action">CCNY</Link>
                 <Link to="#baruch" className="list-group-item list-group-item-action">Baruch</Link>
             </div>
