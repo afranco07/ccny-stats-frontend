367deaa6ae5e4db570d811f1275b936cbba0f983
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-12-27 21:22:42 -0500

    Created page not found and using Switch component in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Homepage from './Homepage/homepage.js';
+import NotFoundPage from './NotFound/notFound.js';
 
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route exact path='/' component={Homepage} />
             <Route exact path='/testpage' component={App} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
