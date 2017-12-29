00efb1eb5aa3964876b8bdcd555be0d40bc672b8
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-12-29 00:28:06 -0500

    Started to get fetch calls to api

--- src/Lists/list.js
+++ src/Lists/list.js
@@ -3,11 +3,46 @@ import { Link } from 'react-router-dom';
 import './listStyles.css';
 
 export default class List extends Component {
+    constructor() {
+        super();
+        this.state = {
+            listItems: [],
+        }
+        this.getSourceLinkItems = this.getSourceLinkItems.bind(this);
+    }
+
+    componentDidMount() {
+        this.getSourceLinkItems();
+    }
+
+    getSourceLinkItems() {
+        fetch("http://localhost:8000/player")
+            .then( response => {
+                return response.json();
+            })
+            .then( jsonBody => {
+                let sourceLinkItems = jsonBody.map( sourceItem => {
+                    return (
+                        <Link to="" className="list-group-item list-group-item-action">
+                            {sourceItem.firstName + " " + sourceItem.lastName}
+                        </Link>
+                    );
+                });
+                this.setState( () => {
+                    return {
+                        listItems: sourceLinkItems,
+                    };
+                });
+            })
+            .catch( () => {
+                console.log("Error converting json object to list items");
+            })
+    }
+
     render() {
         return (
             <div className="list-group" id="list-width">
-                <Link to="#ccny" className="list-group-item list-group-item-action">CCNY</Link>
-                <Link to="#baruch" className="list-group-item list-group-item-action">Baruch</Link>
+                { this.state.listItems }
             </div>
         );
     }
