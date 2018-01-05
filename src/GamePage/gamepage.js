c3d93dd852db538beb57b72441a54f182f1468bf
afranco07 <alberto.franco86@qmail.cuny.edu>
2018-01-04 21:16:07 -0500

    Added the right props to GamePage for the components to function properly

--- src/GamePage/gamepage.js
+++ src/GamePage/gamepage.js
@@ -4,12 +4,28 @@ import AddButton from '../AddButton/addButton.js';
 import Modal from '../Modal/modal.js';
 
 export default class GamePage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            reload: false,
+        };
+        this.reloadPage = this.reloadPage.bind(this);
+    }
+
+    reloadPage() {
+        this.setState( () => {
+            return {
+                reload: !this.state.reload,
+            };
+        });
+    }
+
     render() {
         return (
             <div>
-                <Modal modalTitle="Game" />
+                <Modal modalTitle="Game" reload={this.reloadPage} />
                 <AddButton buttonText="Game" />
-                <List sourceURL="game" />
+                <List sourceURL="game" reload={this.state.reload} reloadList={this.reloadPage} />
             </div>
         );
     }
