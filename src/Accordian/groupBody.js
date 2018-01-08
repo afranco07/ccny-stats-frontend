5671e2e1a8c2f2abb10145dcd41871e3be21768f
afranco07 <alberto.franco86@qmail.cuny.edu>
2018-01-07 22:07:58 -0500

    Added pitch result form into accordian list component

--- src/Accordian/groupBody.js
+++ src/Accordian/groupBody.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import BallOrStrike from '../PitchPage/ballOrStrike.js';
+import PitchResult from '../PitchPage/pitchResult';
+import SubmitPitch from '../PitchPage/submitPitch';
 
 export default class GroupBody extends Component {
     render() {
         return (
             <div id={"collapse" + this.props.groupNumber} className="collapse" role="tabpanel" aria-labelledby={"heading" + this.props.groupNumber} data-parent="#accordian">
                 <div className="card-body">
-                    This is the card body for item #{this.props.groupNumber}
+                    <div className="row">
+                        <div className="col-md col-sm-12">
+                            <BallOrStrike />
+                        </div>
+                        <div className="col-md col-sm-12">
+                            <PitchResult />
+                        </div>
+                        <div className="col-md col-sm-12">
+                            <SubmitPitch />
+                        </div>
+                    </div>
                 </div>
             </div>
         );
