542510cdccf6f62e6711228575b699aac6782005
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2018-02-02 00:04:10 -0500

    Added progress bar to games page

--- src/GamePage/gamepage.js
+++ src/GamePage/gamepage.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import List from '../Lists/list.js';
 import AddButton from '../AddButton/addButton.js';
 import Modal from '../Modal/modal.js';
+import ProgressBar from '../ProgressBar/progressBar.js';
 
 export default class GamePage extends Component {
     constructor() {
         super();
         this.state = {
             reload: false,
+            percentage: 15,
         };
         this.reloadPage = this.reloadPage.bind(this);
+        this.updatePercentage = this.updatePercentage.bind(this);
     }
 
     reloadPage() {
@@ -20,12 +23,21 @@ export default class GamePage extends Component {
         });
     }
 
+    updatePercentage(percent) {
+        this.setState( () => {
+            return {
+                percentage: percent,
+            };
+        });
+    }
+
     render() {
         return (
             <div>
                 <Modal modalTitle="Game" reload={this.reloadPage} />
                 <AddButton buttonText="Game" />
-                <List sourceURL="game" reload={this.state.reload} reloadList={this.reloadPage} />
+                { this.state.percentage < 100 && <ProgressBar percentage={this.state.percentage + "%"} /> }
+                <List sourceURL="game" reload={this.state.reload} reloadList={this.reloadPage} increasePercent={this.updatePercentage} />
             </div>
         );
     }
